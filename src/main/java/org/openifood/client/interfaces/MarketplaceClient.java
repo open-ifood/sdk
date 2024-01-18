cc8fcd455a9054240d36e67d03b4ec77c537bd02
Leonardo Elias <leoelias02@hotmail.com>
2024-01-17 23:52:03 -0300

    :memo: add javadoc to get home client endpoint

--- src/main/java/org/openifood/client/interfaces/MarketplaceClient.java
+++ src/main/java/org/openifood/client/interfaces/MarketplaceClient.java
@@ -8,7 +8,18 @@ import org.openifood.dto.marketplace.response.HomeResponse;
 
 public interface MarketplaceClient {
 
+    /**
+     * List merchants and other card types (items exposed at iFood Home)
+     *
+     * @param auth auth context with access token.
+     * @param request request with params
+     * @param params query params
+     *
+     * @return headers, merchants, products and other card types.
+     */
     @NonNull HomeResponse getHome(@NonNull AuthContext auth, @NonNull HomeRequest request,
                                   @NonNull HomeRequestParams params);
+
+
 
 }
